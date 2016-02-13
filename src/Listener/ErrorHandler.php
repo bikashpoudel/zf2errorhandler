177ea2e2467db836901845c92d75581ff9a9416d
Bikash Poudel <=>
2016-02-13 13:01:17 +0000

    added stuff

---
+++ src/Listener/ErrorHandler.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * @author Bikash Poudel <bikash.poudel.com@gmail.com>
+ */
+
+namespace GrassRoots\ExceptionLogger\Listener;
+
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\ListenerAggregateInterface;
+use Zend\Log\Logger;
+use Zend\Mvc\MvcEvent;
+
+/**
+ * Class ErrorHandler
+ * @package GrassRoots\ExceptionLogger\Listener
+ */
+class ErrorHandler implements ListenerAggregateInterface
+{
+
+    /**
+     * @var array
+     */
+    protected $listeners = array();
+
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    /**
+     * @var string
+     */
+    protected $fatalErrorTemplatePath;
+
+    /**
+     * ErrorHandler constructor.
+     * @param Logger $logger
+     * @param string $fatalErrorTemplatePath
+     */
+    public function __construct(Logger $logger, $fatalErrorTemplatePath)
+    {
+        $this->logger = $logger;
+        $this->fatalErrorTemplatePath = $fatalErrorTemplatePath;
+    }
+
+    /**
+     * @param EventManagerInterface $events
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_BOOTSTRAP, array($this, 'handleNativeErrors'), 2);
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_BOOTSTRAP, array($this, 'handleFatalErrors'), 1);
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'handleExceptions'), -1);
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_RENDER_ERROR, array($this, 'handleExceptions'), -1);
+    }
+
+    /**
+     * @param EventManagerInterface $events
+     */
+    public function detach(EventManagerInterface $events)
+    {
+        foreach ($this->listeners as $index => $listener) {
+            if ($events->detach($listener)) {
+                unset($this->listeners[$index]);
+            }
+        }
+    }
+
+    /**
+     * @param MvcEvent $event
+     */
+    public function handleNativeErrors(MvcEvent $event)
+    {
+        set_error_handler(function ($level, $message, $file, $line) use ($logger) {
+            $minErrorLevel = error_reporting();
+            if ($minErrorLevel & $level) {
+                throw new \ErrorException($message, $code = 0, $level, $file, $line);
+            }
+            // return false to not continue native handler
+            return false;
+        });
+    }
+
+    /**
+     * @param MvcEvent $event
+     */
+    public function handleFatalErrors(MvcEvent $event)
+    {
+        $logger = $this->logger;
+        $templatePath = $this->fatalErrorTemplatePath;
+
+        register_shutdown_function(function () use ($logger, $templatePath) {
+            // get error
+            $error = error_get_last();
+            // check and allow only errors
+            if (null === $error || $error['type'] !== E_ERROR) {
+                return;
+            }
+
+            // clean any previous output from buffer
+            while( ob_get_level() > 0 ) {
+                ob_end_clean();
+            }
+
+            // generate unique reference for this error
+            $chars = md5(uniqid('', true));
+            $errorReference = substr($chars, 2, 2) . substr($chars, 12, 2) . substr($chars, 26, 2);
+
+            $extras = array(
+                'reference' => $errorReference,
+                'file' => $error['file'],
+                'line' => $error['line']
+            );
+
+            // get priority from logger class
+            $priority = Logger::$errorPriorityMap[$error['type']];
+            // log error message and the extra info
+            $logger->log($priority, $error['message'], $extras);
+
+            // read content of file
+            $body = file_get_contents($templatePath);
+            // inject error reference
+            $body = str_replace('%__ERROR_REFERENCE__%', 'Error Reference: ' .  $errorReference, $body);
+            echo $body;
+            die(1);
+        });
+    }
+
+    /**
+     * Handle exceptions
+     *
+     * @param MvcEvent $event
+     */
+    public function handleExceptions(MvcEvent $event)
+    {
+        $logger = $this->logger;
+
+        // check if event is error
+        if (!$event->isError()) {
+            return;
+        }
+        // get message and exception (if present)
+        $message = $event->getError();
+        $exception = $event->getParam('exception');
+        // generate unique reference for this error
+        $chars = md5(uniqid('', true));
+        $errorReference = substr($chars, 2, 2) . substr($chars, 12, 2) . substr($chars, 26, 2);
+        // add it to logger extra array
+        $extras = array(
+            'reference' => $errorReference
+        );
+        // check if event has exception and populate extras array.
+        if (!empty($exception)) {
+            $message =        $exception->getMessage();
+            $extras['file'] =  $exception->getFile();
+            $extras['line']  = $exception->getLine();
+            $extras['trace'] = $exception->getTraceAsString();
+        }
+
+        // log it
+        $priority = Logger::ERR;
+        $logger->log($priority, $message, $extras);
+
+        // hijack skeleton error view and add error reference to the message
+        $originalMessage = $event->getResult()->getVariable('message');
+        $event->getResult()->setVariable('message', $originalMessage . '<br /> Error Reference: ' .  $errorReference);
+    }
+}
---
+++ src/Listener/Service/ErrorHandlerFactory.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author Bikash Poudel <bikash.poudel.com@gmail.com>
+ */
+
+namespace GrassRoots\ExceptionLogger\Listener;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class ErrorHandlerFactory
+ * @package GrassRoots\ExceptionLogger\Listener
+ */
+class ErrorHandlerFactory implements FactoryInterface
+{
+
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return ErrorHandler
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $logger = $serviceLocator->get('GrassRoots\Exception\Logger');
+
+        $resolver     = $serviceLocator->get('Zend\View\Resolver\TemplatePathStack');
+        $templatePath = $resolver->resolve('error/fatal');
+
+        return new ErrorHandler($logger, $templatePath);
+    }
+}
